339c71d6c44db4377e4843f6ddcec3a829e4681a
LanoCodes <del.pow1@gmail.com>
2020-11-29 16:49:16 -0500

    I want to commit the actual testDivide function test

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -8,4 +8,6 @@ public class SimpleCalculator {
 	public int minus(int a, int b){return a - b;}
 
 	public int multiply(int a, int b){return a * b;}
+
+	public int divide(int a, int b){return a / b;}
 }
